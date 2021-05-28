c088bd37918953b31dd5323f8c2ddbdee256ea3c
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-05-28 11:18:42 -0300

    Alterando a versão do JDK

--- src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
@@ -1,2 +1,25 @@
-package academy.devdojo.maratonajava.introducao;public class TiposPrimitivos {
+package academy.devdojo.maratonajava.introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+
+        int age = 10;
+        long numberBig = 100000;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        byte ageByte = -128;
+        short ageShort = 10;
+        boolean trueBoolean = true;
+        boolean falseBoolean = false;
+        char character = '\u0041';
+        char characters = 64;
+
+
+        System.out.println("A idade é "+age+" anos");
+        System.out.println(falseBoolean);
+        System.out.println("char "+character);
+        System.out.println("char "+characters);
+    }
+
 }
