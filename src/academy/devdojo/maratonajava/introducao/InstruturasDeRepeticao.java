d1be7bf0a4be86c627a1704bb79e8c15f32011b0
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-05-28 18:14:24 -0300

    Classe Estrutura de repetição sobre WHILE, DO WHILE e FOR

---
+++ src/academy/devdojo/maratonajava/introducao/InstruturasDeRepeticao.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class InstruturasDeRepeticao {
+    public static void main(String[] args) {
+
+        int count = 1;
+        while (count < 10) {
+            System.out.println(++count);
+        }
+        count = 0;
+        do {
+            System.out.println("Dentro do do-while" + ++count);
+        }
+        while (count < 10);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("for" + i);
+        }
+    }
+}
+
