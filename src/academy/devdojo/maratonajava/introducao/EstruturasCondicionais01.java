902f5ad71ba99fe5edfd0c72d0bc151573c85da8
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-05-28 14:50:37 -0300

    Renomeando classe

--- src/academy/devdojo/maratonajava/introducao/EstruturasCondicionais01.java
+++ src/academy/devdojo/maratonajava/introducao/EstruturasCondicionais01.java
@@ -1,13 +1,15 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class EstruturasCondicionais {
+public class EstruturasCondicionais01 {
     public static void main(String[] args) {
-        int age = 20;
+        int age = 15;
         boolean isAuthorizedBuyDink = age >= 18;
 
         // !
         if (isAuthorizedBuyDink){
             System.out.println("Autorizado a comprar bebida alcólica");
+        }else{
+            System.out.println("Não autorizado a comprar bebida alcólica");
         }
         if (!isAuthorizedBuyDink){
             System.out.println("Não autorizado a comprar bebida alcólica");
