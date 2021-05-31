60d40eea94b18cc1c6bdc5b22d1670de1ed9ba84
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-05-31 11:30:55 -0300

    Classe Arrays

---
+++ src/academy/devdojo/maratonajava/introducao/Arrays01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Arrays01 {
+    public static void main(String[] args) {
+
+        int [] ages = new int[3];
+        ages[0] = 21;
+        ages[1] = 15;
+        ages[2] = 11;
+
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
+    }
+
+}
