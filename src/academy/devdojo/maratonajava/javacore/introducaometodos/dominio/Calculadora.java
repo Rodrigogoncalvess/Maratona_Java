b6107041a68a3e56b286cb7f4a813a0c80578442
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-05-31 16:33:17 -0300

    Orientação objetos métodos com retorno em 3 tipos

--- src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Calculadora.java
@@ -2,20 +2,40 @@ package academy.devdojo.maratonajava.javacore.introducaometodos.dominio;
 
 public class Calculadora {
 
-    public void somaDoisNumeros(){
-        System.out.println(10+10);
+    public void somaDoisNumeros() {
+        System.out.println(10 + 10);
 
     }
-    public void subtrairDoisNumeros(){
-        System.out.println(21-2);
+
+    public void subtrairDoisNumeros() {
+        System.out.println(21 - 2);
     }
-    public  void multiplicaDoisNumeros(int num1, int num2){
-        System.out.println( num1 * num2);
+
+    public void multiplicaDoisNumeros(int num1, int num2) {
+        System.out.println(num1 * num2);
     }
-    public double dividirDoisNumeros(double num1,double num2){
-        if (num2 == 0){
+
+    public double divideDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
             return 0;
         }
-        return num1/num2;
+        return num1 / num2;
+    }
+
+    public double divideDoisNumeros02(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+
+        }
+        return 0;
+    }
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Não existe divisão por zero");
+        return;
+        }
+        System.out.println(num1/num2);
     }
 }
+
--- src/academy/devdojo/maratonajava/javacore/introducaometodos/test/CalculadoraTest03.java
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/test/CalculadoraTest03.java
@@ -7,7 +7,11 @@ public class CalculadoraTest03 {
     public static void main(String[] args) {
 
         Calculadora calculadora = new Calculadora();
-        double result =  calculadora.dividirDoisNumeros(20,2);
+        double result =  calculadora.divideDoisNumeros(20,2);
         System.out.println(result);
+        System.out.println(calculadora.divideDoisNumeros(20,5));
+        calculadora.imprimeDivisaoDeDoisNumeros(86,5);
+
+
     }
 }
