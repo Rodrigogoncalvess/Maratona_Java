141f1792e45faee4803ade29196521ca5e2004e7
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-05-31 18:01:01 -0300

    Orientação objetos métodos referência THIS

--- src/academy/devdojo/maratonajava/javacore/introducaometodos/test/EstudanteTest02.java
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/test/EstudanteTest02.java
@@ -3,12 +3,12 @@ package academy.devdojo.maratonajava.javacore.introducaometodos.test;
 import academy.devdojo.maratonajava.javacore.introducaometodos.dominio.Estudante;
 import academy.devdojo.maratonajava.javacore.introducaometodos.dominio.ImpressoraEstudante;
 
-public class EstudanteTest01 {
+public class EstudanteTest02 {
     public static void main(String[] args) {
 
         Estudante estudante1 =new Estudante();
         Estudante estudante2 =new Estudante();
-        ImpressoraEstudante impressora = new ImpressoraEstudante();
+
 
         estudante1.name = "Midoriya";
         estudante1.age = 15;
@@ -18,8 +18,10 @@ public class EstudanteTest01 {
         estudante2.age = 16;
         estudante2.sex = 'F';
 
-        impressora.imprime(estudante1);
-        impressora.imprime(estudante2);
+        estudante1.imprime();
+        estudante2.imprime();
+
+
 
     }
 }
