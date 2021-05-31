d40e5758f780ff89b6b2e10974637f3d8b44ac05
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-05-31 15:31:10 -0300

    Orientação objetos com métodos

---
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Calculadora.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.introducaometodos.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros(){
+        System.out.println(10+10);
+
+    }
+    public void subtrairDoisNumeros(){
+        System.out.println(21-2);
+    }
+}
