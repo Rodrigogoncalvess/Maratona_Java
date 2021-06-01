f5af07a1c4b5580f75d4dab510cb6a6727487014
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-01 17:35:21 -0300

    Orientação objetos-Bloco de inicialização estático

---
+++ src/academy/devdojo/maratonajava/javacore/modificadorestatico/test/AnimeTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.modificadorestatico.test;
+
+
+import academy.devdojo.maratonajava.javacore.modificadorestatico.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+
+
+
+    }
+}
