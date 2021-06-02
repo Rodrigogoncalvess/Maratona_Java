5e81b620f17c625b0dd0f4db4f814c570858ea23
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-02 17:45:05 -0300

    Orientação objetos - Herança utilizando super

--- src/academy/devdojo/maratonajava/javacore/heranca/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/heranca/dominio/Funcionario.java
@@ -1,9 +1,13 @@
 package academy.devdojo.maratonajava.javacore.heranca.dominio;
 
-public class Funcionario extends Pessoa{
+public class Funcionario extends Pessoa {
     private double salario;
 
-  
+    public void imprime() {
+        super.imprime();
+        System.out.println(this.salario);
+    }
+
     public double getSalario() {
         return salario;
     }
