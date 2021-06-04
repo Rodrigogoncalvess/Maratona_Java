94bd73e6596bfecfbf5d4d7b82158cb63ed4a8db
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-04 12:25:37 -0300

    Orientação objetos - Enumeração

---
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
@@ -0,0 +1,71 @@
+package academy.devdojo.maratonajava.javacore.Kenum.dominio;
+
+public class Cliente {
+    private String nome;
+    private TipoCliente tipoCliente;
+
+    public Cliente(String nome, TipoCliente tipoCliente) {
+        this.nome = nome;
+        this.tipoCliente = tipoCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoCliente getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(TipoCliente tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", tipoCliente=" + tipoCliente +
+                '}';
+    }
+    //    private String tipo;
+
+//    public static final String PESSOA_FISICA = "PESSOA_FISICA";
+//    public static final String PESSOA_JURIDICA = "PESSOA_JURIDICA";
+//
+//    public Cliente(String nome, String tipo) {
+//        if (!tipo.equals(PESSOA_FISICA) && !tipo.equals(PESSOA_JURIDICA)) {
+//            return;
+//        }
+//        this.nome = nome;
+//        this.tipo = tipo;
+//    }
+//
+//    public String getNome() {
+//        return nome;
+//    }
+//
+//    public void setNome(String nome) {
+//        this.nome = nome;
+//    }
+//
+//    public String getTipo() {
+//        return tipo;
+//    }
+//
+//    public void setTipo(String tipo) {
+//        this.tipo = tipo;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Cliente{" +
+//                "nome='" + nome + '\'' +
+//                ", tipo='" + tipo + '\'' +
+//                '}';
+//    }
+}
