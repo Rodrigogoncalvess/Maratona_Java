23bc102b77296573e85637f4a3650a96885a43b8
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-04 14:45:40 -0300

    Orientação objetos - Construtores e atributos

--- src/academy/devdojo/maratonajava/javacore/Kenum/ClienteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/ClienteTest01.java
@@ -5,15 +5,15 @@ import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Pedro", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("João",TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente3 = new Cliente("Pedro",TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("João",TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Pedro", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente2 = new Cliente("João",TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.DEBITO);
+//        Cliente cliente3 = new Cliente("Pedro",TipoCliente.PESSOA_FISICA);
+//        Cliente cliente4 = new Cliente("João",TipoCliente.PESSOA_JURIDICA);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
+//        System.out.println(cliente3);
+//        System.out.println(cliente4);
 
 
 
--- src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
@@ -1,12 +1,19 @@
 package academy.devdojo.maratonajava.javacore.Kenum.dominio;
 
 public class Cliente {
+    
+    public enum TipoPagamento{
+        DEBITO, CREDITO
+    }
+
     private String nome;
     private TipoCliente tipoCliente;
+    private TipoPagamento tipoPagamento;
 
-    public Cliente(String nome, TipoCliente tipoCliente) {
+    public Cliente(String nome, TipoCliente tipoCliente, TipoPagamento tipoPagamento) {
         this.nome = nome;
         this.tipoCliente = tipoCliente;
+        this.tipoPagamento= tipoPagamento;
     }
 
     public String getNome() {
@@ -29,9 +36,12 @@ public class Cliente {
     public String toString() {
         return "Cliente{" +
                 "nome='" + nome + '\'' +
-                ", tipoCliente=" + tipoCliente +
+                ", tipoCliente=" + tipoCliente.getNomeRelatorio() +
+                ", tipoClienteInt=" + tipoCliente.VALOR+
+                ", tipoPagamento=" + tipoPagamento +
                 '}';
     }
+
     //    private String tipo;
 
 //    public static final String PESSOA_FISICA = "PESSOA_FISICA";
