cd93322a762a838ca0d0f3e4aa14c1bf6b9174ee
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-04 11:49:48 -0300

    Orientação objetos - Classes e métodos

--- src/academy/devdojo/maratonajava/javacore/modificadorfinal/dominio/Carro.java
+++ src/academy/devdojo/maratonajava/javacore/modificadorfinal/dominio/Carro.java
@@ -4,8 +4,12 @@ public class Carro {
     private String nome;
 
 
-    public static final double  VELOCIDADE_LIMITE = 250;
+    public static final double VELOCIDADE_LIMITE = 250;
     public final Comprador COMPRADOR = new Comprador();
+
+    public final void imprime() {
+        System.out.println(this.nome);
+    }
 
     public String getNome() {
         return nome;
