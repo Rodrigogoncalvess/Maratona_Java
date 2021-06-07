6fef8c77b303608586a6715d59719941df39ab21
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-07 17:56:48 -0300

    Orientação objetos - Multi catch em linha

--- src/academy/devdojo/maratonajava/javacore/exception/runtime/test/RuntimeExceptionTest04.java
+++ src/academy/devdojo/maratonajava/javacore/exception/runtime/test/RuntimeExceptionTest04.java
@@ -1,35 +1,31 @@
 package academy.devdojo.maratonajava.javacore.exception.runtime.test;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
 
         try {
-             throw new RuntimeException();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Dentro ArrayIndexOutOfBoundsException");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Dentro IndexOutOfBoundsException");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Dentro IllegalArgumentException");
-        } catch (ArithmeticException e) {
-            System.out.println("Dentro ArithmeticException");
+            throw new RuntimeException();
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
+            System.out.println("Dentro ArrayIndexOutOfBoundsException | IllegalArgumentException |ArithmeticException");
+
         } catch (RuntimeException e) {
             System.out.println("Dentro RuntimeException");
         }
 
+
         try {
             talvezLanceException();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } catch (FileNotFoundException e) {
+        } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
+
     }
 
-    private static void talvezLanceException()throws SQLException, FileNotFoundException {
+    private static void talvezLanceException() throws SQLException, IOException {
 
     }
 }
