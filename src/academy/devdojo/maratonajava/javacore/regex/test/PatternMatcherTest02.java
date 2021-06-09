6d4f643e09b3cfd7bbb54a65943c6252e70b757c
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-09 15:30:48 -0300

    Patter e Matcher-Meta caracteres

--- src/academy/devdojo/maratonajava/javacore/regex/test/PatternMatcherTest02.java
+++ src/academy/devdojo/maratonajava/javacore/regex/test/PatternMatcherTest02.java
@@ -3,12 +3,18 @@ package academy.devdojo.maratonajava.javacore.regex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest01 {
+public class PatternMatcherTest02 {
     public static void main(String[] args) {
+// \d = Todos os dígitos
+        // \D = Tudo que não for dígito
+        // \s = Espaço em branco \t \n \f \r
+        // \S = Todos carcteres excluindo os brancos
+        // \w = a-ZA-Z, digitos, -
+        // \W = Tudo que não for incluso no \w
 
-        String regex = "aba";
+        String regex = "\\d";
 //        String texto = "abaaba";
-        String texto2 = "abababa";
+        String texto2 = "@#hh_j2 12gvh21";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto2);
         System.out.println("Texto: " + texto2);
@@ -16,7 +22,7 @@ public class PatternMatcherTest01 {
         System.out.println("regex " + regex);
         System.out.println("Posições encontradas");
         while (matcher.find()) {
-            System.out.print(matcher.start() + " ");
+            System.out.print(matcher.start() + " " + matcher.group() + "\n");
         }
 
 
