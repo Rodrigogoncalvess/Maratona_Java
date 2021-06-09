9402871ef8ef3e6b96b094a02eb23efc06f77a14
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-09 11:02:21 -0300

    TemporalAdjusters

---
+++ src/academy/devdojo/maratonajava/javacore/data/test/TemporalAdjustersTest01.java
@@ -0,0 +1,34 @@
+package academy.devdojo.maratonajava.javacore.data.test;
+
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAdjusters;
+
+public class TemporalAdjustersTest01 {
+    public static void main(String[] args) {
+
+        LocalDate now = LocalDate.now();
+        now = now.with(ChronoField.DAY_OF_MONTH, 9);
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.nextOrSame(DayOfWeek.TUESDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.firstDayOfNextMonth());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.firstDayOfNextYear());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+    }
+}
