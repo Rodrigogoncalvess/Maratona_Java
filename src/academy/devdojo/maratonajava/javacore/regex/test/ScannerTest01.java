f5a91dd21e3e60830ed6d8999926ee2abb86f346
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-09 20:52:02 -0300

    Scanner-Tokens eDelimitadores

--- src/academy/devdojo/maratonajava/javacore/regex/test/ScannerTest01.java
+++ src/academy/devdojo/maratonajava/javacore/regex/test/ScannerTest01.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratonajava.javacore.regex.test;
 public class ScannerTest01 {
     public static void main(String[] args) {
 
-        String texto = "Levi, Eren, Mikasa, true, 200";
+        String texto = "Levi,Eren,Mikasa,true,200";
         String[] nomes = texto.split(",");
         for (String nome: nomes){
             System.out.println(nome.trim());
