6220023e1b283bd819b69033526ccd413001b242
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-15 14:34:11 -0300

    NavigableSet e TreeSet

--- src/academy/devdojo/maratonajava/javacore/colecoes/test/NavigableSetTest01.java
+++ src/academy/devdojo/maratonajava/javacore/colecoes/test/NavigableSetTest01.java
@@ -25,14 +25,27 @@ public class NavigableSetTest01 {
         mangas.add(new Manga(4L, "Hellssing ultimate", 3.2, 0));
         mangas.add(new Manga(3L, "Pokemon", 11.20, 2));
         mangas.add(new Manga(2L, "Dragon ball Z", 2.99, 0));
-        mangas.add(new Manga(2L, "Dragon ball Z", 2.99, 0));
-        mangas.add(new Manga(2L, "Aaragon", 5.99, 0));
+        mangas.add(new Manga(10L, "Aaragon", 5.99, 0));
         mangas.add(new Manga(2L, "Aaragon", 6.99, 0));
 
         for (Manga manga : mangas) {
             System.out.println(manga);
 
         }
+        Manga yuyu = new Manga(21L, "Yuyu Hakusho", 3.2, 5);
+        //lower
+        //floor
+        // higher
+        // ceiling
 
+        System.out.println("--------------");
+        System.out.println(mangas.lower(yuyu));
+        System.out.println(mangas.floor(yuyu));
+        System.out.println(mangas.higher(yuyu));
+        System.out.println(mangas.ceiling(yuyu));
+
+        System.out.println(mangas.size());
+        System.out.println(mangas.pollFirst());
+        System.out.println(mangas.pollLast());
     }
 }
