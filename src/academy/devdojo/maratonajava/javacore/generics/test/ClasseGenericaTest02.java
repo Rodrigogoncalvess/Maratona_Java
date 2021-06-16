2aedab05854e3ed3b8ad7fa3550b9b7f2829073b
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-16 09:22:42 -0300

    Classes genericas

---
+++ src/academy/devdojo/maratonajava/javacore/generics/test/ClasseGenericaTest02.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.generics.test;
+
+import academy.devdojo.maratonajava.javacore.generics.dominio.Barco;
+import academy.devdojo.maratonajava.javacore.generics.service.BarcoRentavelService;
+
+public class ClasseGenericaTest02 {
+    public static void main(String[] args) {
+        BarcoRentavelService barcoRentavelService = new BarcoRentavelService();
+        Barco barco = barcoRentavelService.buscarBarcoDisponivel();
+        System.out.println("Usando o barco por um mês ...");
+        barcoRentavelService.retornarBarcoAlugado(barco);
+
+    }
+}
