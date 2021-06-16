93022cfa755c9a60fe9ab21acb21aacc2f791ef0
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-16 14:47:35 -0300

    Method Reference - Referência a métodos não estáticos

---
+++ src/academy/devdojo/maratonajava/javacore/lambda/test/MethodReferenceTest02.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.lambda.test;
+
+import academy.devdojo.maratonajava.javacore.lambda.dominio.Anime;
+import academy.devdojo.maratonajava.javacore.lambda.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodReferenceTest02 {
+    public static void main(String[] args) {
+        AnimeComparators animeComparators = new AnimeComparators();
+       List<Anime> animeList =new ArrayList<>( List.of(new Anime("Berserk", 43),new Anime("One piece", 900),new Anime("Naruto", 500)));
+//        animeList.sort(animeComparators::compareByEpisodesNoStatic);
+        animeList.sort((a1,a2)-> animeComparators.compareByEpisodesNoStatic(a1,a2));
+        System.out.println(animeList);
+    }
+}
